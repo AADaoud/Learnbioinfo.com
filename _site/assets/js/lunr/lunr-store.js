b3f37c7874d10102d413fc35a58b371579ade6cf
AADaoud <97793913+AADaoud@users.noreply.github.com>
2022-09-22 17:39:05 +0300

    main

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,32 +1,20 @@
 var store = [{
-        "title": "1 - Welcome to statistics",
-        "excerpt":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...","categories": [],
-        "tags": [],
-        "url": "/biostatistics/intro.html",
-        "teaser": null
-      },{
-        "title": "2 - Remember highschool math class?",
-        "excerpt":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...","categories": [],
-        "tags": [],
-        "url": "/foundational-info/",
-        "teaser": null
-      },{
         "title": "3 - Hypothesis testing: One sample",
         "excerpt":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...","categories": [],
         "tags": [],
         "url": "/biostatistics/HT1S.html",
         "teaser": null
       },{
-        "title": "4 - Hypothesis testing: Two samples",
-        "excerpt":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...","categories": [],
-        "tags": [],
-        "url": "/biostatistics/HT2S.html",
-        "teaser": null
-      },{
         "title": "5 - Hypothesis testing: All the samples! (>2)",
         "excerpt":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...","categories": [],
         "tags": [],
         "url": "/biostatistics/HT2+S.html",
+        "teaser": null
+      },{
+        "title": "4 - Hypothesis testing: Two samples",
+        "excerpt":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...","categories": [],
+        "tags": [],
+        "url": "/biostatistics/HT2S.html",
         "teaser": null
       },{
         "title": "6 - Hypothesis testing: Categorical outcomes",
@@ -39,6 +27,18 @@ var store = [{
         "excerpt":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...","categories": [],
         "tags": [],
         "url": "/biostatistics/HTNP.html",
+        "teaser": null
+      },{
+        "title": "2 - Remember highschool math class?",
+        "excerpt":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...","categories": [],
+        "tags": [],
+        "url": "/foundational-info/",
+        "teaser": null
+      },{
+        "title": "1 - Welcome to statistics",
+        "excerpt":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...","categories": [],
+        "tags": [],
+        "url": "/biostatistics/intro.html",
         "teaser": null
       },{
         "title": "8 - Linear regressions and their uses",
